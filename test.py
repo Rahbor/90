6364149f6a79f86bfcbba770507714d09b04908a
Rahbor <115787033+Rahbor@users.noreply.github.com>
2022-10-14 16:46:27 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+age= input("Whats your age: ")
+int_age= int(age)
+years = 24 - int_age
+days = years *365
+weeks = years * 52
+months = years*12
+
+print(f"You still have {months} months, {weeks} weeks,{days} days left to hit 90 years old ")
